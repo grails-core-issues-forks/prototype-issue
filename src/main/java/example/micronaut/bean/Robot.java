1b91427307eca8480d5aef7a85cf0b4706b895e3
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-14 13:56:58 +0200

    change @Bean Robot

--- src/main/java/example/micronaut/bean/Robot.java
+++ src/main/java/example/micronaut/bean/Robot.java
@@ -2,22 +2,20 @@ package example.micronaut.bean;
 
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.core.annotation.NonNull;
-import io.micronaut.http.HttpRequest;
-import io.micronaut.runtime.http.scope.RequestAware;
 
-import java.util.Objects;
+import java.util.UUID;
 
 @Bean
-public class Robot implements RequestAware {
-    private String serialNumber;
+public class Robot {
+    @NonNull
+    private final String serialNumber;
+
+    public Robot() {
+        serialNumber = UUID.randomUUID().toString();
+    }
 
     @NonNull
     public String getSerialNumber() {
         return serialNumber;
-    }
-
-    @Override
-    public void setRequest(HttpRequest<?> request) {
-        serialNumber = Objects.requireNonNull(request.getHeaders().get("UUID"));
     }
 }
